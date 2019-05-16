1b786272ee7119fe1c98a7ed61b8e05d7e197347
bgp2112 <bpalmer@nvidia.com>
2019-05-16 11:05:02 -0700

    initial

---
+++ Test10-ListScan.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+./ListScan_Solution -e ./ListScan/Dataset/0/output.raw -i ./ListScan/Dataset/0/input.raw -t vector
+./ListScan_Solution -e ./ListScan/Dataset/1/output.raw -i ./ListScan/Dataset/1/input.raw -t vector
+./ListScan_Solution -e ./ListScan/Dataset/2/output.raw -i ./ListScan/Dataset/2/input.raw -t vector
+./ListScan_Solution -e ./ListScan/Dataset/3/output.raw -i ./ListScan/Dataset/3/input.raw -t vector
+./ListScan_Solution -e ./ListScan/Dataset/4/output.raw -i ./ListScan/Dataset/4/input.raw -t vector
+./ListScan_Solution -e ./ListScan/Dataset/5/output.raw -i ./ListScan/Dataset/5/input.raw -t vector
+./ListScan_Solution -e ./ListScan/Dataset/6/output.raw -i ./ListScan/Dataset/6/input.raw -t vector
+./ListScan_Solution -e ./ListScan/Dataset/7/output.raw -i ./ListScan/Dataset/7/input.raw -t vector
+./ListScan_Solution -e ./ListScan/Dataset/8/output.raw -i ./ListScan/Dataset/8/input.raw -t vector
+./ListScan_Solution -e ./ListScan/Dataset/9/output.raw -i ./ListScan/Dataset/9/input.raw -t vector
+
