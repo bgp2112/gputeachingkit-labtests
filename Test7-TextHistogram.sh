1100d137346fac9a4c63ff8ac8ec2cb78e5ff843
Brad Palmer <30506267+bgp2112@users.noreply.github.com>
2019-12-17 09:54:29 -0500

    Update Test7-TextHistogram.sh

--- Test7-TextHistogram.sh
+++ Test7-TextHistogram.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-./TextHistogram_Solution -e ./TextHistogram/Dataset/0/output.raw -i ./TextHistogram/Dataset/0/input.txt -t integral_vector
-./TextHistogram_Solution -e ./TextHistogram/Dataset/1/output.raw -i ./TextHistogram/Dataset/1/input.txt -t integral_vector
-./TextHistogram_Solution -e ./TextHistogram/Dataset/2/output.raw -i ./TextHistogram/Dataset/2/input.txt -t integral_vector
-./TextHistogram_Solution -e ./TextHistogram/Dataset/3/output.raw -i ./TextHistogram/Dataset/3/input.txt -t integral_vector
-./TextHistogram_Solution -e ./TextHistogram/Dataset/4/output.raw -i ./TextHistogram/Dataset/4/input.txt -t integral_vector
-./TextHistogram_Solution -e ./TextHistogram/Dataset/5/output.raw -i ./TextHistogram/Dataset/5/input.txt -t integral_vector
+if [ -z $1 ]; then
+    builddir="."
+else
+    builddir=$1
+fi
+$builddir/TextHistogram_Solution -e $builddir/TextHistogram/Dataset/0/output.raw -i $builddir/TextHistogram/Dataset/0/input.txt -t integral_vector
+$builddir/TextHistogram_Solution -e $builddir/TextHistogram/Dataset/1/output.raw -i $builddir/TextHistogram/Dataset/1/input.txt -t integral_vector
+$builddir/TextHistogram_Solution -e $builddir/TextHistogram/Dataset/2/output.raw -i $builddir/TextHistogram/Dataset/2/input.txt -t integral_vector
+$builddir/TextHistogram_Solution -e $builddir/TextHistogram/Dataset/3/output.raw -i $builddir/TextHistogram/Dataset/3/input.txt -t integral_vector
+$builddir/TextHistogram_Solution -e $builddir/TextHistogram/Dataset/4/output.raw -i $builddir/TextHistogram/Dataset/4/input.txt -t integral_vector
+$builddir/TextHistogram_Solution -e $builddir/TextHistogram/Dataset/5/output.raw -i $builddir/TextHistogram/Dataset/5/input.txt -t integral_vector
 
